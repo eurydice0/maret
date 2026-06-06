ecfba11ff24f7a5abacf5d775f3c91a4118ed220
eurydice0 <riokuncoro26@gmail.com>
2026-06-06 18:30:10 +0700

    UPDATE

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 function Header() {
   return (
     <header>
-      <video src="/video.mp4" loop autoPlay muted></video>
-      <video src={process.env.PUBLIC_URL + "/video.mp4"} loop autoPlay muted></video>
+      <video 
+        src="/video.mp4" 
+        loop 
+        autoPlay 
+        muted
+        style={{width:'100%', height:'100%', objectFit:'cover'}}
+      ></video>
       <h1 style={{ color: "#0057B8", fontFamily: "Montserrat, sans-serif", fontWeight: "800", letterSpacing: "4px", textTransform: "uppercase" }}>PT Indomarco Prismatama</h1>
-
       <div className="headerbg"></div>
     </header>
   );
